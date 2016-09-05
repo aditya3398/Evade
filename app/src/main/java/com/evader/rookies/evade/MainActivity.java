c87ff465207dfe698ddaace1c22d6552e25e06a6
Vishwa Shah <vshah0350@gmail.com>
2016-09-05 00:56:52 -0400

    Home screen, colors, drawables

--- app/src/main/java/com/evader/rookies/evade/MainActivity.java
+++ app/src/main/java/com/evader/rookies/evade/MainActivity.java
@@ -1,7 +1,9 @@
 package com.evader.rookies.evade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //called when button on home screen is clicked
+    public void playGame(View view) {
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
+    }
+
 }
