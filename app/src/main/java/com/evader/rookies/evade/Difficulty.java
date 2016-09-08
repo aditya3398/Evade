45fe795e0a4e4eeeb889e2959b09f66976914d20
Shukan Shah <Shukan Shah>
2016-09-08 03:10:54 -0400

    Added ability to change game difficulty

---
+++ app/src/main/java/com/evader/rookies/evade/Difficulty.java
@@ -0,0 +1,83 @@
+package com.evader.rookies.evade;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+
+/**
+ * Created by shuka on 9/7/2016.
+ */
+public class Difficulty extends Activity {
+
+    String difficulty = "";
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        setContentView(R.layout.activity_change_difficulty);
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+
+
+        difficulty = preferences.getString("DifficultyLevel", "");
+
+    }
+
+    public void onEasy(View view){
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString("DifficultyLevel","Easy");
+        editor.apply();
+
+        goingBack();
+
+    }
+    public void onMedium(View view){
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString("DifficultyLevel","Medium");
+        editor.apply();
+
+        goingBack();
+
+    }
+    public void onHard(View view){
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString("DifficultyLevel","Hard");
+        editor.apply();
+
+        goingBack();
+
+    }
+
+    public void goingBack(){
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        difficulty = preferences.getString("DifficultyLevel", "");
+
+        Intent goingBack = new Intent();
+
+        goingBack.putExtra("DifficultyLevel", difficulty);
+        setResult(RESULT_OK, goingBack);
+
+        finish();
+
+    }
+}
--- app/src/main/java/com/evader/rookies/evade/MainActivity.java
+++ app/src/main/java/com/evader/rookies/evade/MainActivity.java
@@ -1,22 +1,71 @@
 package com.evader.rookies.evade;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String difficulty = "";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+
+
+        difficulty = preferences.getString("DifficultyLevel", "");
+
+        if(difficulty.equals("")){
+
+            editor.putString("DifficultyLevel","Medium");
+            editor.apply();
+            difficulty = preferences.getString("DifficultyLevel", "");
+
+
+
+        }
+
     }
 
     //called when button on home screen is clicked
     public void playGame(View view) {
-        Intent intent = new Intent(this, Game.class);
-        startActivity(intent);
+
+
+        Intent startingGameIntent = new Intent(this, Game.class);
+        startingGameIntent.putExtra("DifficultyLevel", difficulty);
+        startActivity(startingGameIntent);
     }
+
+    public void changeDifficulty(View view){
+        Intent changingDifficultyIntent = new Intent(this, Difficulty.class);
+        final int result = 1;
+
+        startActivityForResult(changingDifficultyIntent, result);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        difficulty = data.getStringExtra("DifficultyLevel");
+        Toast.makeText(this, "Changed difficulty to " + difficulty, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+
+
 
 }
